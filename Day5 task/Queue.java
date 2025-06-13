12da750bde69197f65ce3191ff3ea5076f8ddaed
CAS EXAM <casexam@kgisledu.com>
2025-06-13 10:19:39 +0530

    updated

---
+++ Day5 task/Queue.java
@@ -0,0 +1,57 @@
+package task_day5;
+
+import java.util.Arrays;
+
+public class  Queue {
+	int arr[]=new int[5];
+	int rear;
+	int front;
+	public Queue()
+	{
+	      rear=-1;
+	      front=-1;
+	}
+	public void enqueue(int num) {
+		if(rear==4)
+		{
+			System.out.println("Queue spreadout");
+			
+			}
+		else
+		{
+			arr[++rear]=num;
+		}
+	}
+		
+     public void dequque() {
+    	 if(front==4)
+    	 {
+    		 System.out.println("Empty queue");
+    	}
+    	 else
+    	 {
+    		 arr[++front]=0;
+    	 }
+		
+	}
+     public void display()
+     {
+    	 System.out.println(Arrays.toString(arr));
+     }
+     
+     public static void main(String[] args) {
+    	 Queue q = new Queue();
+    	 q.enqueue(2);
+    	 q.enqueue(4);
+    	 q.enqueue(6);
+    	 q.enqueue(8);
+    	 q.enqueue(5);
+    	 q.enqueue(3);
+    	 q.display();
+     }
+	
+	      
+		
+	}
+
+
---
+++ Day5 task/Stack.java
@@ -0,0 +1,55 @@
+package task_day5;
+
+public class Stack {
+        int mystack[]=new int[5];
+        int top=-1;
+        
+        public void push(int num) {
+        	if(top==0)
+        	{
+        		System.out.println("Stack overflow");
+        	}
+        	else
+        	{
+        		mystack[++top]=num;
+        	
+        	}
+        }
+        public void pop()
+        {
+        	if(top<0) 
+        	{
+        		System.out.println("Underflow");
+        		
+        	}
+        	else
+        		System.out.println(mystack[top]+" popped out");
+        	--top;
+        }
+        public void display()
+        {
+        	for(int i=0;i<mystack.length;i++)
+        	{
+        		System.out.println(mystack[i]+"");
+        	}
+        }
+
+  
+	  public static void main(String[] args) {
+	        Stack s = new Stack();
+	        s.push(2);
+	        s.push(9);
+	        s.push(6);
+	        s.push(1);
+	        s.pop();
+	        s.pop();
+	        s.display();
+	  }
+	        
+           
+      	  
+        
+	  
+	  }
+	  
+  
